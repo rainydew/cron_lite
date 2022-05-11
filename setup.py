a748fbf174820c74d88453579b1f4164b61f7ae3
Rainy Chan <rainydew@github.com>
2022-05-12 02:43:29 +0800

    1.0

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 from distutils.core import setup
-from os import path, chdir, system
+from os import path, system
 from sys import argv
 
 if "upload" in argv:
-    chdir("json_compare")
     print("running test")
-    assert system("python test_json_compare.py") == 0
-    chdir("..")
+    assert system("python test_cron_lite.py") == 0
 
 this_directory = path.abspath(path.dirname(__file__))
 
@@ -26,8 +24,8 @@ setup(
     author_email='rainydew@qq.com',
     url='https://github.com/rainydew/cron_lite',
     py_modules=["cron_lite"],
-    install_requires=['six>=1.12.0'],
-    keywords='json comparison order unicode fuzzy',
+    install_requires=['croniter>=1.3.4'],
+    keywords='cron task decorator schedule',
     long_description=long_description,
     python_requires=">=3.6"
 )
